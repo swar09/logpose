401ba49eb9a1ec49b335a5e0ed14691bcb40aead
swar09 <ghuleswarnit@gmail.com>
2026-08-03 17:46:01 +0530

    feat: add update timestamp functionality with triggers for users and urls tables

--- migrations/2026-07-27-133056-0000_create_users/down.sql
+++ migrations/2026-07-27-133056-0000_create_users/down.sql
@@ -3,3 +3,4 @@ DROP TABLE url_analytics;
 DROP TABLE urls;
 DROP TABLE users;
 DROP TYPE transaction_status;
+DROP FUNCTION IF EXISTS update_updated_at_column;
--- migrations/2026-07-27-133056-0000_create_users/up.sql
+++ migrations/2026-07-27-133056-0000_create_users/up.sql
@@ -1,3 +1,11 @@
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
 CREATE TABLE users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   first_name VARCHAR(100) NOT NULL,
@@ -8,6 +16,12 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+CREATE TRIGGER update_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
+
 CREATE TABLE urls (
   database_id SERIAL PRIMARY KEY ,
   short_code VARCHAR(4) UNIQUE,
@@ -15,8 +29,14 @@ CREATE TABLE urls (
   created_by UUID NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  CONSTRAINT fk_urls FOREIGN KEY (created_by) REFERENCES users(id)
+  CONSTRAINT fk_urls FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TRIGGER update_urls_updated_at
+    BEFORE UPDATE ON urls
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
+
 CREATE TABLE url_analytics(
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   short_code VARCHAR(4) NOT NULL,
@@ -27,8 +47,9 @@ CREATE TABLE url_analytics(
   device VARCHAR(100),
   country_code VARCHAR(10),
   referer VARCHAR(2048),
-  CONSTRAINT fk_url_analytics FOREIGN KEY (short_code) REFERENCES urls(short_code)
+  CONSTRAINT fk_url_analytics FOREIGN KEY (short_code) REFERENCES urls(short_code) ON DELETE CASCADE
 );
+
 CREATE TYPE transaction_status AS ENUM ('pending', 'success', 'failed');
 CREATE TABLE transactions(
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -38,7 +59,8 @@ CREATE TABLE transactions(
   status transaction_status NOT NULL,
   reference_id UUID ,
   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  CONSTRAINT fk_transactions FOREIGN KEY (user_id) REFERENCES users(id)
+  CONSTRAINT fk_transactions FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 -- CREATE TABLE subscribtions();
+
  
